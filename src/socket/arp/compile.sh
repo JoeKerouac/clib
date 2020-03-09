d793f9d4154764fa357748d9f420debb0a48e2a9
JoeKerouac <guanhua.qgh@antfin.com>
2020-03-09 12:18:23 +0800

    更新编译脚本

--- src/socket/arp/compile.sh
+++ src/socket/arp/compile.sh
@@ -1,2 +1,3 @@
-cp -f arp_request_lib.h /usr/include/arp_request_lib.h
+rm -rf /usr/include/arp_request_lib.h
+ln -s arp_request_lib.h /usr/include/arp_request_lib.h
 gcc -fPIC -shared arp_request_lib.c -o /usr/lib/libarp_request_lib.so
