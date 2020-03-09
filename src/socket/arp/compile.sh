e3b01a48d6f13d7ea4d7f61daac547a4b3445aa4
JoeKerouac <guanhua.qgh@antfin.com>
2020-03-09 13:46:33 +0800

    1

--- src/socket/arp/compile.sh
+++ src/socket/arp/compile.sh
@@ -1,3 +1,3 @@
 rm -rf /usr/include/arp_request_lib.h
-ln -s arp_request_lib.h /usr/include/arp_request_lib.h
+cp arp_request_lib.h /usr/include/arp_request_lib.h
 gcc -fPIC -shared arp_request_lib.c -o /usr/lib/libarp_request_lib.so
