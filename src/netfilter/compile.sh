c392a96bf53f19c55523ff957147d79c2df52297
JoeKerouac <1213812243@qq.com>
2020-11-17 22:05:50 +0800

    更新类库编译命令

--- src/netfilter/compile.sh
+++ src/netfilter/compile.sh
@@ -1,2 +1,6 @@
+# 安装nf_userspace_queue库到本机
+cp nf_userspace_queue.h /usr/include/nf_userspace_queue.h
+gcc -fPIC -shared nf_userspace_queue.c -o /usr/lib/libnf_userspace_queue.so
+# 编译本目录下的测试类
 gcc -c nf_userspace_queue.c
 gcc test.c nf_userspace_queue.o -l mnl -l netfilter_queue -o main
